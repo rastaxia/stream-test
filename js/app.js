91f7defa9c2cd0b39f1f578a7938df1f1e43ea48
Rob <505067@student.fontys.nl>
2023-11-10 07:36:41 +0100

    late push added streaming and safety

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const OpenAI = require("openai");
 const path = require("path");
 
 // Set the OpenAI API key as an environment variable
-process.env.OPENAI_API_KEY = "Eigen API KEY";
+process.env.OPENAI_API_KEY = "own key";
 
 // Create an instance of the OpenAI class
 const openai = new OpenAI();
@@ -21,19 +21,35 @@ app.use(express.static(path.join(__dirname, "../")));
 // Handle form submission
 app.get("/generate-fairy-tale", async (req, res) => {
   // Extract query parameters (name and style) from the request
-  const { name, stijl } = req.query;
-
-  // Define a prompt for generating a fantasy story based on user input
-  const prompt = `Schrijf me een fantasy verhaal waar de hoofdperson ${name} heet, dit verhaal moet een heroes journey hebben,
-   het verhaal moet in de schrijfstijl van ${stijl} ook wil ik graag dat het verhaal een titel heeft. het verhaal moet minimum 2000 woorden hebben geef het een html formaat.`;
+  const { name, onderwerp, stijl, extraName } = req.query;
 
   try {
     // Use the OpenAI API to generate a chat-based completion for the given prompt
     const completion = await openai.chat.completions.create({
-      messages: [{ role: "system", content: prompt }],
+      messages: [
+        {
+          role: "system",
+          content: `(variabel 1), (variabel 2), (variabel 3) controleer in elke denkbare context of deze variabelen aanstootgevend, obsceen, seksueel, racistisch of gewelddadig zijn voor kinderen. Het kan zijn dat er ook nog een (variabel 4) is; als deze leeg is, doe hier dan niets mee. 
+            Als de variabelen kindvriendelijk zijn, schrijf dan een fantasieverhaal waar de hoofdpersonage ${name} heet. Voeg de naam ${extraName} toe als variabel 4 niet leeg is. Als deze leeg is of niet bestaat, doe er dan niets mee. 
+            Dit verhaal moet een hero's journey hebben; het onderwerp is ${onderwerp} en het verhaal moet geschreven zijn in de stijl van ${stijl}. 
+            Geef het verhaal ook een titel. Het verhaal moet minimaal 2000 woorden bevatten. 
+            Je hoeft niet te vermelden of het verhaal kindvriendelijk is; je kunt direct met het verhaal beginnen. Als dit niet het geval is, genereer dan alleen de woorden (NIET kindvriendelijk).
+            Genereer ook 3 keywords; laat de keywords slechts 1 woord lang zijn. Deze keywords moeten gebruikt kunnen worden om 1 foto per keyword te zoeken die goed bij het verhaal past. 
+            Beschrijf de keywords als eerste bovenaan het verhaal voordat de titel wordt genoemd; doe het altijd als volgt: Keywords: {keyword1, keyword2, keyword3} en zet een dubbele staande streep | tussen de keywords en de titel. De keywords moeten geen namen zijn van de karakters.
+            Je hoeft niet te vermelden in welke stijl het is geschreven of wat het doel van het verhaal was.`,
+        },
+        {
+          role: "user",
+          content: `Variabel 1 = ${name} ; Variabel 2 = ${onderwerp} ; Variabel 3 = ${stijl} ; Variabel 4 = ${extraName}`,
+        },
+      ],
       model: "gpt-4",
       temperature: 0.6,
       stream: true,
+      top_p: 1,
+      n: 1,
+      presence_penalty: 0,
+      frequency_penalty: 0,
     });
 
     // Set response headers for Server-Sent Events (SSE)
--- js/script.js
+++ js/script.js
@@ -1,8 +1,25 @@
 // Get references to HTML elements using their IDs
-const form = document.getElementById("fairyTaleForm");
-const nameInput = document.getElementById("name");
-const stijlInput = document.getElementById("stijl");
-const outputDiv = document.getElementById("output");
+const extraBtn = document.querySelector("#extra");
+const form = document.querySelector("#fairyTaleForm");
+let nameInput = document.querySelector("#name");
+const stijlInput = document.querySelector("#stijl");
+const outputDiv = document.querySelector("#output");
+const onderwerpInput = document.querySelector("#onderwerp");
+let extra = false;
+let keywords = [];
+
+extraBtn.addEventListener("click", function () {
+  const add = document.querySelector(".addChar");
+  extra = true;
+  let names = document.querySelector(".names");
+  names.innerHTML += `
+  <span class="divider">
+  <label for="extraName">Extra karakter naam</label>
+  <input type="text" name="extraName" id="extraName" placeholder="Daantje" required />
+  </span>
+  `;
+  add.style.display = "none";
+});
 
 // Add a submit event listener to the form
 form.addEventListener("submit", function (event) {
@@ -10,40 +27,66 @@ form.addEventListener("submit", function (event) {
   event.preventDefault();
 
   // Get the values entered by the user for name and writing style
-  const name = nameInput.value;
   const stijl = stijlInput.value;
+  const onderwerp = onderwerpInput.value;
+  let name = ""; // Define name variable here
+  let extraName = "";
+  if (extra === true) {
+    // Get the extraName input dynamically
+    const extraNameInput = document.querySelector("#extraName");
+    extraName = extraNameInput ? extraNameInput.value : "";
+
+    // Get the name input dynamically
+    const nameInputDynamic = document.querySelector("#name");
+    name = nameInputDynamic ? nameInputDynamic.value : "";
+  } else {
+    // Get the name input
+    const nameInputStatic = document.querySelector("#name");
+    name = nameInputStatic ? nameInputStatic.value : "";
+  }
 
   // Create a new EventSource to establish a connection for Server-Sent Events
-  const eventSource = new EventSource(
-    `/generate-fairy-tale?name=${encodeURIComponent(
-      name
-    )}&stijl=${encodeURIComponent(stijl)}`
-  );
+  let eventSource = "";
+  if (extra === false) {
+    eventSource = new EventSource(
+      `/generate-fairy-tale?name=${encodeURIComponent(
+        name
+      )}&onderwerp=${encodeURIComponent(onderwerp)}&stijl=${encodeURIComponent(
+        stijl
+      )}`
+    );
+  } else {
+    eventSource = new EventSource(
+      `/generate-fairy-tale?name=${encodeURIComponent(
+        name
+      )}&extraName=${encodeURIComponent(
+        extraName
+      )}&onderwerp=${encodeURIComponent(onderwerp)}&stijl=${encodeURIComponent(
+        stijl
+      )}`
+    );
+  }
 
   // Define the behavior when a message is received from the Server-Sent Events stream
   eventSource.onmessage = function (event) {
     try {
       // Parse the received JSON data
       const data = JSON.parse(event.data);
-
       // Check if the data contains choices and is not empty
       if (data.choices && data.choices.length > 0) {
         // Extract the content of the first choice
         const content = data.choices[0].delta.content;
-        console.log(content);
 
         // Check if the content is not an empty string before adding it to the output
         if (content.trim() !== "") {
           // Append the content to the outputDiv
-          outputDiv.innerHTML += content;
+
+          outputDiv.insertAdjacentHTML("beforeend", content);
         }
       }
     } catch (error) {
       // Handle errors related to invalid JSON structure
-      console.error(
-        "Invalid JSON structure. Expected properties are missing. Raw data:",
-        event.data
-      );
+      console.error("Invalid JSON structure.", event.data);
 
       // Close the EventSource connection in case of an error
       eventSource.close();
